f52f8a8cb4d66dcf578997b175416c9816efe803
Ege Sucu <iletisim@egesucu.com>
2019-02-13 19:25:09 +0300

    Initial Commit

---
+++ TableviewController 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableviewController 1
+//
+//  Created by Ege Sucu on 2019-02-13.
+//  Copyright © 2019 Ege Sucu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
